5a43c0709ef2bcc0688f854e0fa4a26771f3717f
Albert Cervin <albert.cervin@frostbite.com>
2018-08-03 16:32:47 +0200

    🎉 Initial commit

---
+++ ElmAsset.js
@@ -0,0 +1,44 @@
+const elmCompiler = require('node-elm-compiler');
+const findAllDependencies = require("find-elm-dependencies").findAllDependencies;
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const Asset = require('parcel-bundler/src/Asset');
+
+class ElmAsset extends Asset {
+
+  constructor(name, options) {
+    super(name, options);
+    this.type = 'js';
+  }
+
+  getParserOptions() {
+    const defaultOptions = {
+      yes: true,
+      cwd: process.cwd(),
+    };
+    return defaultOptions;
+  }
+  
+  async getDependencies() {
+    let deps = await findAllDependencies(this.name);
+    deps.forEach(dep => {
+      this.addDependency(dep, { includedInParent: true });
+    });
+  }
+
+  async generate() {
+    const options = this.getParserOptions();
+    const data = await elmCompiler.compileToString(this.name, options);
+
+    return [
+      {
+        type: 'js',
+        value: data.toString()
+      }
+    ];
+  }
+}
+
+module.exports = ElmAsset;
+
